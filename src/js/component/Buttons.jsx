c0d81a45c6054702c5e5668541b429925c0f7e33
Felipe Coste <32721311+felipecoste@users.noreply.github.com>
2024-10-28 19:49:18 +0000

    Se agrega cuenta regresiva, proyecto terminado

--- src/js/component/Buttons.jsx
+++ src/js/component/Buttons.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-const Buttons = ({pauseCount, stopCount, resumeCount}) => {
+const Buttons = ({restartCount, stopCount, resumeCount}) => {
 
     return (
         <>
             <div className="container d-flex justify-content-center">
                 <div className="btn-group btn-group-lg" role="group" aria-label="Large button group">
-                    <button type="button" className="btn btn-outline-secondary text-light" onClick={pauseCount}>Pause</button>
+                    <button type="button" className="btn btn-outline-secondary text-light" onClick={restartCount}>Restart</button>
                     <button type="button" className="btn btn-outline-secondary text-light" onClick={stopCount}>Stop</button>
                     <button type="button" className="btn btn-outline-secondary text-light" onClick={resumeCount}>Resume</button>
                 </div>
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,16 +6,48 @@ import Buttons from "./Buttons";
 const Home = () => {
 	const [count, setCount] = useState(0);
 	const [isRunning, setIsRunning] = useState(true);
+	const [isCountdown, setIsCountdown] = useState(false);
+	const [countdownValue, setCountdownValue] = useState(0);
 
+
+	// Formato de 6 digitos
+	const sixDigitsFormat = (count) => {
+		const formatoContador = count.toString().padStart(6, "0");
+		return formatoContador.split('');
+	}
+
+
+	// Contador
 	useEffect(() => {
 		if (isRunning) {
 			const interval = setInterval(() => {
-				setCount(count + 1);
-			}, 500);
+				setCount(prevCount => {
+					// Si es cuenta regresiva, resta el valor
+					if (isCountdown) {
+						if (prevCount > 0) {
+							return prevCount - 1;
+						} else {
+							clearInterval(interval);
+							setIsRunning(false);
+							setIsCountdown(false); // Detener cuenta regresiva
+							return 0;
+						}
+					}
+					// Si no es cuenta regresiva, incrementa el valor
+					return prevCount + 1;
+				});
+			}, 1000);
 			return () => clearInterval(interval);
 		}
-	}, [count]
-	)
+	}, [isRunning, isCountdown]);
+
+
+	// Funciones de los botones
+	const restartCount = () => {
+		setCount(0);
+		setIsCountdown(false);
+		setIsRunning(true);
+	}
 
 	const stopCount = () => {
 		setIsRunning(false);
@@ -26,24 +58,49 @@ const Home = () => {
 	}
 
 
-
-
-	const sixDigitsFormat = (count) => {
-		const formatoContador = count.toString().padStart(6, "0");
-		return formatoContador.split('');
+	// Función para manejar el input
+	const handleCountdownChange = (e) => {
+		setCountdownValue(e.target.value);
+	}
+	// Iniciar regresiva
+	const startCountdown = () => {
+		setCount(countdownValue);
+		setIsCountdown(true);
+		setIsRunning(true);
 	}
 
 	return (
 		<>
 			<div className="card container border border-0 bg-black mt-5 rounded-5">
 				<div className="card-body">
+					<h1 className="text-white text-center mt-4"> Simple Counter </h1>
 
 					<SecondCounter digits={sixDigitsFormat(count)} />
+
 					<Buttons
-						//pauseCount={pauseCount}
+						restartCount={restartCount}
 						stopCount={stopCount}
 						resumeCount={resumeCount}
 					/>
+
+					<h5 className="text-white text-center mt-4 mb-3">Cuenta Regresiva</h5>
+					<div className="container d-flex justify-content-center mb-5">
+						<input
+							type="number"
+							className="form-control me-2"
+							placeholder="Ingrese una cantidad en segundos"
+							style={{ width: "40vw" }}
+							onChange={handleCountdownChange}
+						/>
+						<button
+							className="btn btn-outline-secondary text-light"
+							type="button"
+							onClick={startCountdown}
+						>
+							Iniciar
+						</button>
+					</div>
+
 
 				</div>
 			</div>
